0c42349beb6f17587c6700c370fa4330bb507fdc
GawnGyu Yeo <bbc02131@naver.com>
2022-11-15 11:33:12 +0900

    @RequestMapping & @RequestParam

--- helloweb03/src/main/java/com/bitacademy/helloweb/controller/UserController.java
+++ helloweb03/src/main/java/com/bitacademy/helloweb/controller/UserController.java
@@ -1,27 +1,52 @@
 package com.bitacademy.helloweb.controller;
 
+import java.lang.ProcessBuilder.Redirect;
+
+import org.eclipse.jdt.internal.compiler.ast.FalseLiteral;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * 
  * @RequestMapping 클래스+메소드
+ * 추천
  *
  */
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-	@ResponseBody
-	@RequestMapping("/joinform")
-	public String joinfrom() {
-		return "UserController.joinfrom()";
+	@RequestMapping(value = "/join",method = RequestMethod.GET)
+	public String join() {
+		return "/WEB-INF/views/join.jsp";
 	}
 
+	@RequestMapping(value = "/join",method = RequestMethod.POST )
+	public String join(UserVo vo) {
+		System.out.println(vo);
+		return "redirect:/";
+	}
 	@ResponseBody
-	@RequestMapping("/join")
-	public String join() {
-		return "UserController.join()";
+	@RequestMapping("/update")
+	public String update(@RequestParam("n") String name) {
+		/**
+		 * 만일 n이라는 이름의 URL파라미터가 없는경우
+		 * 400Bad Request 에러가 발생샇낟.
+		 */
+		return "UserController.update("+name+")";
+	}
+	@ResponseBody
+	@RequestMapping("/update")
+	public String update2(@RequestParam(value = "n", required = true, defaultValue = "") String name) {
+		return "UserController.update("+name+")";
+	}
+	@ResponseBody
+	@RequestMapping("/list")
+	public String list(@RequestParam(value = "p", required = true, defaultValue = "1") int pageNo) {
+		return "UserController.list("+pageNo+")";
 	}
 }
